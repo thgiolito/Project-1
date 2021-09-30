bb77184024464fe995eb911b927e624df8c878ed
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2021-09-30 11:23:23 +0200

    Added bestiary HTML CSS JS

---
+++ script.js
@@ -0,0 +1,132 @@
+const beastToDestroy = [
+    // {
+    // name: "Oeuf d'alien",
+    // picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    // hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    // detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    // protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    // },
+    {
+    name: "Chupa cabra",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Zombaby",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Arachnide",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Samara",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Sinok",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Noiraude",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Data Analyst",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Demogorgon",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "It",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Chucky",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+    name: "Chtulu",
+    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
+    detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    }
+];
+  
+const beasts = document.querySelector(".beasts");
+
+function createBeast(title, imageUrl) {
+    const beast = document.createElement("div");
+    beast.classList.add("beast");
+    beasts.appendChild(beast);
+
+    const beastImg = document.createElement("img");
+    beastImg.style.backgroundImage = `url(${imageUrl})`;
+    beastImg.classList.add("beast-img");
+    beast.appendChild(beastImg);
+
+    const beastName = document.createElement("h2");
+    beastName.classList.add("beast-name");
+    beastName.innerHTML = title;
+    beast.appendChild(beastName);
+}
+
+for (let i = 0; i < beastToDestroy.length; i++) {
+    createBeast(beastToDestroy[i]["name"], beastToDestroy[i]["picture"]);
+}
+
+const beastDetail = document.querySelector(".beastDetail");
+const beastclic = document.querySelector(".beast");
+beastclic.addEventListener('click', function(){
+    beastDetail.style.display = "flex"; 
+    beasts.style.display = "none"; 
+  })
+
+beastDetail.addEventListener('click', function(){
+    beastDetail.style.display = "none"; 
+    beasts.style.display = "flex"; 
+  })  
