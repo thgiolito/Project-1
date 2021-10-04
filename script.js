02ef9927ad6e52fb035ef8bd7b2f9cee344c0645
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2021-10-04 16:46:31 +0200

    Add responsive design

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const beastToDestroy = [
     picture: "https://st.depositphotos.com/thumbs/1795881/image/2347/23476086/api_thumb_450.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    protection: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description: "Protège un infâme crabe surnommé calineur de visage qui sussure des 'Miss, t'es charmante' dès qu'une femme passe à proximité. Risque d'attaque faciale, s'accroche à l'aide de ses pattes arachnéennes en quémandant un zéro-six.",
+    protection: "Curez-vous le nez ostensiblement, cela devrait les maintenir à distance un moment.",
   },
   {
     name: "Chupa cabra",
@@ -13,9 +13,9 @@ const beastToDestroy = [
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Créature fantastique sans poil ayant des yeux rouges et une bouche avec des crocs saillants vers le haut et vers le bas. Se nourrit en suçant le sang des animaux de ferme, en faisant des trous parfaitement circulaires dans le cou jusqu'au cervelet.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Eloignez au plus vite vos animaux de ferme : veaux, vaches, cochons, chèvre...",
   },
   {
     name: "Data Analyst",
@@ -23,79 +23,79 @@ const beastToDestroy = [
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Responsable du recueil et de l’analyse de la donnée d’une entreprise, le data analyst communique avec ses pairs par télépathie et voue un culte au Big Data auquel il sacrifie des tables de données les soirs de pleine lune.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Mettez un casque anti-bruit et tenez-vous hors de portée de ses ondes mentales. L'efficacité du chapeau en aluminium reste à prouver.",
   },
   {
     name: "Zombaby",
-    picture: "https://st2.depositphotos.com/thumbs/1606463/image/8079/80792684/api_thumb_450.jpg",
+    picture: "https://st4.depositphotos.com/thumbs/1006593/image/27793/277937394/api_thumb_450.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Créature rampante parfois équipée de bras, le zombaby est un être pleurnichard qui se nourrit de cerve-lait.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Evitez tout contact avec sa bouche, l'éventuelle absence de dent ne la rend pas moins dangereuse. Si possible, attrapez-le avec une pince à biberon et enfermez-le dans un parc.",
   },
   {
     name: "Arachnide",
-    picture: "https://images.pexels.com/photos/2665108/pexels-photo-2665108.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    picture: "https://st4.depositphotos.com/thumbs/5946506/image/20003/200038184/api_thumb_450.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Créature extra-terrestre ovipare très agressive ressemblant à un insecte géant. Les arachnides vivent en communautés régies sous l'absolue dictature de leur reine et sont en conflit ouvert avec la Fédération terrienne.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Recouvrez-vous des excréments que vous venez d'expulser, creusez un trou dans le sol et cachez-vous dedans.",
   },
   {
-    name: "Samara",
+    name: "Sadako",
     picture: "https://st3.depositphotos.com/thumbs/1006542/image/12890/128904568/api_thumb_450.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Esprit vengeur vêtu d'une chemise de nuit blanche et fuyant les salons de coiffure. Lorsqu'elle était humaine, elle fut assassinée et jetée dans un puits. Depuis, elle se demande ce qu'on a tous à lui mettre la rage.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Eteignez la télévision et placez-vous à l'intérieur d'un cercle protecteur constitué de peignes.",
   },
   {
-    name: "Sinok",
-    picture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
+    name: "Lego®",
+    picture: "https://images.assetsdelivery.com/compings_v2/korolkoff/korolkoff2002/korolkoff200200065.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Petite brique de construction en plastique de couleur. Apparaît spontanément dans les foyer équipés d'enfants ou d'adulescents et se glisse insidieusement sous leurs pieds.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Mettez des chaussures à semelle rigide et équipez-vous de sachets zippables.",
   },
   {
     name: "Noiraude",
-    picture: "https://st.depositphotos.com/thumbs/1787196/image/1330/13301832/api_thumb_450.jpg",
+    picture: "https://images.assetsdelivery.com/compings_v2/kouzins/kouzins1903/kouzins190300016.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Petites boules de suie d'à peine quelques centimètres de diamètre que l'on retrouve généralement par petits paquets amassés. Elles n'ont pas de personnalité et peuvent être comparées à un groupe d'insectes.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Ouvrez les volets, allumez vos lampes à U.V. et riez très fort.",
   },
   {
-    name: "Demogorgon",
+    name: "Démogorgon",
     picture: "https://st3.depositphotos.com/thumbs/7477192/image/17597/175972944/api_thumb_450.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Créature humanoïde grande et mince à la peau visqueuse. Ses membres digitigrades allongés se terminent par des griffes et sa tête s'ouvre sur une immense bouche entourée de pétales dentés sentant la pivoine.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Jetez-lui un Raider®, un walkman ou un mulet. Demandez-lui s'il est de la famille d'Ahmon-Ibor, le Prince des Démons, Seigneur de Tout ce qui Nage dans les Ténèbres.",
   },
   {
-    name: "It",
+    name: "Ça",
     picture: "https://images.unsplash.com/photo-1601513445498-5dbffc8d5d5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Entité extraterrestre métamorphe éternelle et plus ancienne que l'univers prenant généralement la forme d'un clown. Egalement connue sous le nom de Pennywise, elle cible les enfants de manière cyclique.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Jetez-lui des enfants ou des balles de jonglage.",
   },
   {
     name: "Chucky",
@@ -103,28 +103,27 @@ const beastToDestroy = [
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Poupée possédée par l'esprit d'un tueur en série grâce à un rituel vaudou. Fasciné par les grands couteaux de cuisine.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Passez-lui quelques épisodes de Ken le survivant, jetez-lui des allumettes (allumées).",
   },
   {
-    name: "Ch'tulu",
+    name: "Chtulhu",
     picture: "https://img2.freepng.fr/20180511/bxw/kisspng-the-rats-in-the-walls-the-call-of-cthulhu-the-cats-5af61c86287cd0.6832657115260785981658.jpg",
     hiddenPicture: "https://i.ibb.co/BNwhCDP/ImgTD2.png",
     detailPicture: "https://i.ibb.co/M2kb2y4/ImgTD1.png",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Gigantesque créature extraterrestre de forme vaguement humanoïde à tête de seiche, tentacules de pieuvre et ailes de dragon. Théoriquement endormie depuis des millénaires dans la cité de Phoo à R'lyeh engloutie sous l'océan Pacifique. Souvent accompagnée de culturistes.",
     protection:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Fuyez, pauvres fous !",
   },
 ];
 
 const beasts = document.querySelector(".beasts");
 
-function createBeast(title, imageUrl, index) {
+function createBeast(title, imageUrl) {
   const beast = document.createElement("div");
   beast.classList.add("beast");
-  beast.id = index;
   beasts.appendChild(beast);
 
   const beastImg = document.createElement("div");
@@ -139,7 +138,7 @@ function createBeast(title, imageUrl, index) {
 }
 
 for (let i = 0; i < beastToDestroy.length; i++) {
-  createBeast(beastToDestroy[i]["name"], beastToDestroy[i]["picture"], i);
+  createBeast(beastToDestroy[i]["name"], beastToDestroy[i]["picture"]);
 }
 
 function fillBeastCard(title, image, presentation, protection) {
@@ -158,15 +157,16 @@ function fillBeastCard(title, image, presentation, protection) {
 }
 
 const beastDetail = document.querySelector(".beast-detail");
+const bestiaryTitle = document.querySelector(".bestiary-title");
 
 const beastclic = document.querySelectorAll('.beast');
 for (let i = 0; i < beastclic.length; i++) {
   beastclic[i].addEventListener("click",  function(){
     const item = beastToDestroy[i]
-    console.log(item);
-    fillBeastCard(item.name, item.detailPicture, item.description, item.protection)
+    fillBeastCard(item.name, item.picture, item.description, item.protection)
     beastDetail.style.display = "flex";
     beasts.style.display = "none";
+    bestiaryTitle.style.display = "none";
   })
 }
 
@@ -175,4 +175,21 @@ const quitButton = document.querySelector(".quit-button");
 quitButton.addEventListener("click", function () {
   beastDetail.style.display = "none";
   beasts.style.display = "flex";
+  bestiaryTitle.style.display = "block";
 });
+
+// const leftButton = document.querySelector(".left-button");
+// for (let i = 0; i < beastToDestroy.length; i++) {
+//   leftButton.addEventListener("click", function () {
+//     const item = beastToDestroy[i-1]
+//     fillBeastCard(item.name, item.picture, item.description, item.protection)
+//   })
+// }
+
+// const rightButton.document.querySelector(".right-button");
+// for (let i = 0; i < beastToDestroy.length; i++) {
+//   rightButton.addEventListener("click", function () {
+//     const item = beastToDestroy[i-1]
+//     fillBeastCard(item.name, item.picture, item.description, item.protection)
+//   })
+// }
