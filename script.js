dbdd02b35922ceb8395ad3ef7848b24ba9b8f695
thgiolito <thgiolito@gmail.com>
2021-10-05 16:06:57 +0200

    working version 1.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("#form");
+const form = document.querySelector("#contactForm");
 const monsters = {
     it: {
         intervenants: 3,
@@ -26,18 +26,26 @@ form.onsubmit = function(event){
 
     const intervenantsResult = document.getElementById('intervenantsResult');
     const teamNeeded = document.createElement('div');
-    teamNeeded.classList.add('result');
     teamNeeded.innerHTML = `${monsters[monsterType].intervenants * monsterNumber/2}`;
     intervenantsResult.appendChild(teamNeeded);
 
+    let currentDate = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    
+    function addDays(currentDate, days) {
+        return new Date(currentDate.getTime() + days*24*60*60*1000);
+    }
+
+    const result = addDays(currentDate, userWhen).toLocaleDateString(undefined, options);
     const dateResult = document.getElementById('dateResult');
-    const dateNeeded = document.createElement('div');
+    dateResult.innerHTML = `${result}`;
+
 
     const priceResult = document.getElementById('priceResult');
     priceResult.innerHTML = `${monsters[monsterType].price * monsterNumber}`;
    
 
-
+    console.log(result);
     console.log(monsterNumber);
     console.log(monsterType);
     console.log(userWhen);
